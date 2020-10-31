ed3e9694c6cb2f62978b8f34de1d88fea937919e
Jordi Baliellas <jordibal98@gmail.com>
2020-10-31 21:34:26 +0100

    create parts mock

---
+++ backend/src/mocks/parts.mock.ts
@@ -0,0 +1,21 @@
+import { Part } from '../models/part.model';
+
+export const partsMock: Part[] = [
+  {
+    partId: 1,
+    name: 'tornillo',
+    features: [
+      {
+        name: 'feature 1',
+      },
+    ],
+  },
+  {
+    partId: 2,
+    name: 'clavo',
+  },
+  {
+    partId: 3,
+    name: 'arandela',
+  },
+];
