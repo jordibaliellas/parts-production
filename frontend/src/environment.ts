821adfc28df121c2b650c7699b27377dac1a0756
Jordi Baliellas <jordibal98@gmail.com>
2020-10-31 21:40:22 +0100

    change files js to ts and connect front to back with sockets

---
+++ frontend/src/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  backendApi: process.env.REACT_APP_BACKEND_API || 'http://localhost:3030',
+  socketEndpoint: process.env.REACT_APP_BACKEND_API || 'http://localhost:3030',
+};
