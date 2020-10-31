f001412e835aa06f8595f6c07b2ab888f688b6ab
Jordi Baliellas <jordibal98@gmail.com>
2020-10-31 21:37:50 +0100

    create part sockets and rooms

---
+++ backend/src/sockets/part.socket.ts
@@ -0,0 +1,27 @@
+import { getPart } from '../controllers/part.controller';
+import { Part } from '../models/part.model';
+
+export const partSocket = (io: SocketIO.Server) => {
+  setInterval(() => {
+    const totalClients = Object.keys(io.sockets.adapter.rooms).reduce(
+      (pv, room) => pv + io.sockets.adapter.rooms[room].length,
+      0
+    );
+
+    if (totalClients === 0) return;
+
+    Object.keys(io.sockets.adapter.rooms).forEach((room) => {
+      const part: Part = getPart(room);
+      if (!part) return;
+
+      io.sockets.in(room).emit('message', JSON.stringify(part));
+    });
+  }, 3000);
+
+  return (socket: SocketIO.Socket) => {
+    socket.on('part', (part) => {
+      socket.leaveAll();
+      socket.join(`part-${part}`);
+    });
+  };
+};
