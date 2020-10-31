9aeb5cbd65ea8446ef4dcd74314bca8e1baa6d19
Jordi Baliellas <jordibal98@gmail.com>
2020-10-31 21:38:19 +0100

    call route parts and socket parts from index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,30 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import SocketIo from 'socket.io';
+import { getParts } from './routes/parts.router';
+import { partSocket } from './sockets/part.socket';
 const app = express();
+app.use(cors());
+const server = http.createServer(app);
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+const io = SocketIo(server);
+
+app.get('/parts', getParts);
+
+io.on('connection', partSocket(io));
+
+io.on('connection', (socket) => {
+  // tslint:disable-next-line: no-console
+  console.log('New client connected');
+
+  socket.on('disconnect', () => {
+    // tslint:disable-next-line: no-console
+    console.log('Client disconnected');
+  });
 });
 
-app.listen(3030, () => {
+server.listen(3030, () => {
   // tslint:disable-next-line: no-console
-  console.log("Server running in http://localhost:3030");
+  console.log('Server running in http://localhost:3030');
 });
