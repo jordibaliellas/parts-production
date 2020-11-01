73f5c4c6357752e3245f9420d66decfe29a75db9
Jordi Baliellas <jordibal98@gmail.com>
2020-11-01 16:14:27 +0100

    call all components from app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,42 +3,64 @@ import socketIOClient from "socket.io-client";
 import { Part } from '../../backend/src/models/part.model';
 import { environment } from './environment';
 import './App.css';
+import {Selector, ItemListSelector} from './components/Selector/Selector';
+import Feature from './components/Feature/Feature';
+import NoDataPart from './components/No-data-part/No-data-part';
 
 function App() {
-  const [parts, setParts] = useState<Part[]>([]);
-  let socket: SocketIOClient.Socket;
-  socket = socketIOClient(environment.socketEndpoint);
-  socket.on('message', (data: string) => {
-    console.log('Incoming message:', data);
-  });
+  const [partsList, setPartsList] = useState<ItemListSelector[]>([]);
+  const [socket, setSocket] = useState<SocketIOClient.Socket>();
+  const [part, setPart] = useState<Part>();
+  
+  let FeatureBody = <NoDataPart></NoDataPart>;
+  FeatureBody = part ? <div>{part.features.map((feature, i) => 
+    (<Feature key={feature.name + '-' + i} feature={feature}></Feature>)
+  )}</div> : <NoDataPart></NoDataPart>; 
 
   useEffect(() => {
     fetch(`${environment.backendApi}/parts`, {headers: {'Access-Control-Allow-Origin': '*',}, referrerPolicy: "no-referrer"})
       .then(response => response.json())
-      .then(data => setParts(data));
+      .then((parts: Part[]) => parts.map(partItem => ({label: partItem.name, value: partItem.partId.toString()})))
+      .then((data: ItemListSelector[]) => setPartsList(data));
+    
+    console.log('connect to socket');
+
+    const connectionSocket = socketIOClient(environment.socketEndpoint)
+
+    setSocket(connectionSocket);
+    connectionSocket.on('message', (data: string) => {
+      console.log('Incoming message:', data);
+      setPart(JSON.parse(data));
+    });
   }, []);
 
-
-  const handleChange = (item: ChangeEvent<HTMLSelectElement>) => {
-    console.log('parts', parts);
-
+  const handleChange = (item: ChangeEvent<HTMLSelectElement>): void => {
     const value = item.target.value;
-    if (value === '0') return;
 
-    socket.emit('part', value);
+    const newPartsList = partsList.map(partItem => {
+      if (partItem.value === value) partItem.selected = true;
+      else partItem.selected = false;
+      return partItem;
+    });
 
-    console.log('chaangee select');
-    console.log(item.target.value);
+    setPartsList(newPartsList);
+
+    if (socket) {
+      setPart(undefined);
+      socket.emit('part', value);
+    }
   }
 
   return (
-    <select onChange={handleChange}>
-      <option value="0">Select part</option>
-
-      {parts.map((part, i) => 
-         (<option key={part.partId} value={part.partId}>{part.name}</option>)
-      )}
-    </select>
+    <div>
+      <div style={{justifyContent:'center', display: 'flex', padding: '1rem'}}>
+        <Selector handleChange={handleChange} optionDefault='Select part' list={partsList}></Selector>
+      </div>
+      {/* <TitleFeature title={titleFeature} icon={icon} backgroundColor={backgroundColor}></TitleFeature>
+      <ControlsFeature controls={(part && part.features && part.features[0] && part.features[0].controls) || []}></ControlsFeature>
+      <FooterFeature></FooterFeature> */}
+      {FeatureBody}
+    </div>
   );
 }
 
