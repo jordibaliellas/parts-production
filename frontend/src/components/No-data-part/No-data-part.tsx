d9f39621c82325607691280eb5ef65272868dd72
Jordi Baliellas <jordibal98@gmail.com>
2020-11-01 16:13:49 +0100

    create component no data part

---
+++ frontend/src/components/No-data-part/No-data-part.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NoDataPart() {
+
+  return (
+    <div style={{ padding: '1rem', backgroundColor: '#E4E4E4', color: '#484848', display: 'flex', justifyContent: 'center' }}>
+      Select Part please
+    </div>
+  );
+}
+
+export default NoDataPart;
